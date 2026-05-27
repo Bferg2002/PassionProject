314728afdb0ccf92f472fbad27e96e6796a703a8
Bryant Ferguson <btbf569708@gmail.com>
2026-05-27 17:19:12 -0400

    Inital project setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+// Import your custom components here, for example:
+// import MyComponent from './components/MyComponent';
+
+function App() {
+  return (
+    <div className="app-container">
+      <header>
+        <h1>My React Application</h1>
+      </header>
+
+      <main>
+        {/* Paste your component calls below this line */}
+        
+        {/* <MyComponent /> */}
+
+        {/* Paste your component calls above this line */}
+      </main>
+
+      <footer>
+        <p>&copy; {new Date().getFullYear()} My App</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
